79d3740c057b64f2d45bf6dda8eabb29f2c6188c
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-04-21 22:57:52 +0300

    The configurations for Telegram and the corresponding settings have been removed from the initialization file

--- Source/__init__.py
+++ Source/__init__.py
@@ -21,22 +21,14 @@ config: Dict[str, Dict[str, Any]] = {
     "Webhooks": {
         "PrivateSectorFeed": os.getenv("WEBHOOK_PRIVATE_SECTOR_FEED"),
         "GovermentFeed": os.getenv("WEBHOOK_GOVERNMENT_FEED"),
-        "RansomwareFeed": os.getenv("WEBHOOK_RANSOMWARE_FEED"),
-        "TelegramFeed": os.getenv("WEBHOOK_TELEGRAM_FEED"),
         "StatusMessages": os.getenv("WEBHOOK_STATUS_MESSAGES"),
-    },
-    "Telegram": {
-        "BotName": os.getenv("TELEGRAM_BOT_NAME"),
-        "APIID": os.getenv("TELEGRAM_API_ID"),
-        "APIHash": os.getenv("TELEGRAM_API_HASH"),
-        "ImageDownloadFolder": os.getenv("TELEGRAM_IMAGE_DOWNLOAD_FOLDER", "TelegramImages"),
     },
     "RSS": {
         "RSSLogFile": os.getenv("RSS_LOG_FILE", "RSSLog.txt"),
     }
 }
 
-for section in ["Webhooks", "Telegram"]:
+for section in ["Webhooks"]:
     if section not in config:
         sys.exit(f'Please specify a "{section}" section in the config file')
 
